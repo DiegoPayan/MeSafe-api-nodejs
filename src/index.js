c35dd793e6f374d67db957aa086531a773bec744
DiegoPayan <dapayanl@concredito.com.mx>
2019-05-11 15:59:52 -0600

    Conexion con servidor clever cloud

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.json());
 
 //Routes
-app.use('/login', require('./routes/users'));
+app.use('/login', require('./routes/usuarios'));
 
 //Starting the server
 app.listen(app.get('port'), () => {
