da2dd823abcb9f44c4e39fc11fce512dd1da944f
diegopayan <diegogmr1335@gmail.com>
2019-05-12 20:26:52 -0600

    Centro de ayuda

--- src/database/meSafe.sql
+++ src/database/meSafe.sql
@@ -110,3 +110,37 @@ CREATE TABLE IF NOT EXISTS calificacionReportes (
 
 ALTER TABLE calificacionReportes ADD FOREIGN KEY (idReporte) REFERENCES reportes(id);
 ALTER TABLE calificacionReportes ADD FOREIGN KEY (idUsuario) REFERENCES usuarios(id);
+
+CREATE TABLE IF NOT EXISTS tipoCentroAyuda (
+    id INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+INSERT INTO tipoCentroAyuda(nombre) VALUES ("Hospital"), ("Bomberos"), ("Policias"), ("Instituciones");
+
+CREATE TABLE IF NOT EXISTS centroAyuda (
+    id INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    telefono VARCHAR(15) NOT NULL,
+    direccion VARCHAR(200) NOT NULL,
+    idTipoCentroAyuda INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE centroAyuda ADD FOREIGN KEY (idTipoCentroAyuda) REFERENCES tipoCentroAyuda(id);
+
+INSERT INTO centroAyuda(nombre,telefono,direccion,idTipoCentroAyuda) 
+                VALUES  ("Hospital Civil","758 0500","Álvaro Obregón 1422, Tierra Blanca, 80030 Culiacán Rosales, Sin.", 1),
+                        ("Hospital Angeles","758 7700","Av Gral Ignacio Aldama S/N, Guadalupe, 80230 Culiacán Rosales, Sin.", 1),
+                        ("Hospital General","716 9815","Blvrd Alfonso G. Calderón 2139, Country Alamos, Culiacán Rosales, Sin.", 1),
+                        ("Estacion #1 Bomberos","712 3199","Gabriel Leyva 444 Oriente, Centro, 80000 Culiacán Rosales, Sin.", 2),
+                        ("Estacion #2 Bomberos","714 5750","Avenida Emiliano Zapata 3536, Francisco Villa, 80110 Culiacán Rosales, Sin.", 2),
+                        ("Estacion #3 Bomberos","753 4520","Av. Álvaro Obregón 273, 6 de Enero, 80010 Culiacán Rosales, Sin.", 2),
+                        ("Fiscalia General del Estado","713 3200","Enrique Sánchez Alonso 1833, Desarrollo Urbano Tres Ríos, 80100 Culiacán Rosales, Sin.", 3),
+                        ("Policia de Investigacion","714 2833","Av. Álvaro Obregón 273, 6 de Enero, 80010 Culiacán Rosales, Sin.", 3),
+                        ("Servicio de emergencia","911","Culiacan Sin.", 3),
+                        ("Instituto Municipal de las mujeres","716 9757","Calle Cristóbal Colón 245, Primer Cuadro, 80000 Culiacán Rosales, Sin.", 4),
+                        ("DIF","715 7142","Juan José Ríos, Poniente #265-B, Jorge Almada, 80200 Culiacán Rosales, Sin.", 4),
+                        ("Instituto Estatal de protección civil","717 8287","Primera 65, Vallado Viejo, 80110 Culiacán Rosales, Sin.", 4);
+
